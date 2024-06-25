4697061c5117bb2fb3aecd9a615ed880c81f4bda
Alex <alexeigolovkov@gmail.com>
2024-06-26 01:53:54 +0700

    Start working

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/practica/TPU_novel
+# Build directory: C:/practica/TPU_novel/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int, char**){
+    printf("Hell, from TPU_novel!\n");
+}
